fd685118d44405843df3cd2acdefa23ce41c4c3e
INS140 <IsaacNSaettel@gmail.com>
2023-01-09 23:01:10 -0800

    admin portal update

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,33 @@
+let root = document.querySelector('#root')
 
-// Your Code Here
+const updateQuantity = async () => {
+    let res = await fetch('http://localhost:3001/listBooks')
+    let books = await res.json()
+
+    let ul = document.createElement('ul')
+
+    books.forEach(e => {
+        let li = document.createElement('li')
+
+        let input = document.createElement('input')
+        input.type = 'text'
+        input.value = e.quantity
+
+        let btn = document.createElement('button')
+        btn.textContent = 'Save'
+        btn.addEventListener('click', () => {
+            fetch('http://localhost:3001/updateBook', {
+                method: 'PATCH',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({'id': e.id, 'quantity': `${input.value}`})
+            })
+        })
+        
+        li.append(e.title, input, btn)
+        ul.append(li)
+    })
+
+    root.append(ul)
+}
+
+updateQuantity()
